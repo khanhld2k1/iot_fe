f14a98e27f537ffa95ee2fb79b1fbe03ae49ab54
KhanhLD2K1 <nagadark111@gmail.com>
2024-08-28 11:18:58 +0700

    Some change 2

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense, ReactElement, PropsWithChildren } from 'react';
-import { Outlet, RouteObject, RouterProps, createBrowserRouter } from 'react-router-dom';
+import { Outlet, RouteObject, RouterProps, createBrowserRouter, Router } from 'react-router-dom';
 
 import PageLoader from 'components/loading/PageLoader';
 import Splash from 'components/loading/Splash';
@@ -76,6 +76,8 @@ const options: { basename: string } = {
   basename: '/nickelfox',
 };
 
-const router: Partial<RouterProps> = createBrowserRouter(routes, options);
+// const router: Partial<RouterProps> = createBrowserRouter(routes, options);
+// const router: Router = createBrowserRouter(routes, options);
+const router = createBrowserRouter(routes, options);
 
 export default router;
